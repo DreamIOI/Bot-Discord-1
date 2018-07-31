0a7a0deda2ac0a935dfb8bc455f29166f03968dd
DreamIOI <me.nghiem.cuong@gmail.com>
2018-07-31 23:16:10 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,4 +4,4 @@ const client = new Discord.Client();
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
-client.login(process.env.BOT_TOKEN);
+client.login('NDcxNjY0OTEwNzA3ODUxMjc0.DjoKSA.BGw2GOt7scurflS25iWQcOxsorE');
